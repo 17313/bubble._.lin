9fa8e1be87f99649f7dafc92522e5854e751ac60
17313 <17313@burnside.school.nz>
2020-06-19 08:45:25 +1200

    it is what it is

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def close_connection(exception):
 def index():
     return render_template('index.html')
 
-@app.route('/admin', methods=["GET", "POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
     error = None
     if request.method == 'POST':
@@ -32,29 +32,27 @@ def login():
             error = "Invalid credentials. Please try again"
         else:
             session['logged_in'] = True
+            flash("Ypu were just logged in")
             return redirect("/")
-    return render_template("admin.html", error=error)
+    return render_template("login.html", error=error)
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
+    flash("You were just logged out")
     return redirect("/")
-
-
-
-
 
 @app.route('/menu')
 def menu():
     return render_template("menu.html")
 
-@app.route("/order")
-def order():
-    sql = "SELECT * FROM 'order';"
+@app.route("/drink_order")
+def order():   
+    sql = "SELECT * FROM 'drink_order';"
     cursor = get_db().cursor()
     cursor.execute(sql)
     results = cursor.fetchall()
-    return render_template('order.html' , results=results)
+    return render_template('drink_order.html' , results=results)
 
 @app.route("/add", methods={"GET", "POST"})
 def add():
@@ -64,20 +62,25 @@ def add():
         drink_name = request.form['drink_name']
         drink_temperature = request.form["drink_temperature"]
         drink_additives = request.form["drink_additives"]
-        sql = "INSERT INTO 'order'(username, name, temperature, additives) VALUES (?,?,?,?)"
+        sql = "INSERT INTO 'drink_order'(username, name, temperature, additives) VALUES (?,?,?,?)"
         cursor.execute(sql,(customer_name, drink_name, drink_temperature, drink_additives))
         get_db().commit()
-    return redirect("/order")
+    return redirect("/drink_order")
 
 @app.route('/delete' , methods=["GET", "POST"])
 def delete():
-    if request.method == "POST":
-        cursor = get_db().cursor()
-        id = int(request.form["username"])
-        sql = "DELETE FROM 'order' WHERE id=?"
-        cursor.execute(sql,(id,))
-        get_db().commit()
-    return redirect('/order')
+    error = None
+    if request.method == 'POST':
+        if request.form['password'] != 'order':
+            error = "Invalid credentials. Please try again"
+        else:
+            cursor = get_db().cursor()
+            id = int(request.form["username"])
+            sql = "DELETE FROM 'drink_order' WHERE id=?"
+            cursor.execute(sql,(id,))
+            get_db().commit()
+    return redirect('/drink_order')
+
 
 
 if __name__ == "__main__":
