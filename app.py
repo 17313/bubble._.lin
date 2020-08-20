cc51bdb84db2f79403b68699ca3924b0d8877ae3
17313 <moneyshotzgamer@gmail.com>
2020-08-21 10:54:41 +1200

    improving old mistakes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, g, request, redirect, session, flash, session
+
+from flask import Flask, render_template, g , request, redirect, session, flash
 import sqlite3
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -6,57 +7,51 @@ app = Flask(__name__)
 DATABASE = 'Bubble._.Lin.db'
 global admin
 admin = False
+global user_id
+user_id = None
+global username
+username = 
+
 
 app.secret_key = 'bubbles'
 
-# get database
+
 def get_db():
+    '''get database'''
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
-# closes connection to database
 @app.teardown_appcontext
 def close_connection(exception):
+    '''closes connection to database'''
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
     return
 
-# create an app route
-@app.route('/')
-def index():
-    # link to index html template
-    return render_template('index.html')
-
-'''def check_user(username, password):
-    # get the user from the database
-    user = 'user'
-    if user and check_password_hash(user[1], password):
-        session['user'] = user[0] # store the user id in session
-        return True
-    return False
-
-def create_user(username, password):
-    password_hashed = generate_password_hash(password)
-    # add to database
-    sql = "INSERT INTO login(password) VALUES ?"
-    cursor.execute(sql,(password))
-
 def get_user():
-    user_id = session['user']
-    if user_id:
-        # get from the database the user by id
+    if session.get("user"):
+        '''get user from database'''
+        user = "SELECT username FROM drink_order WHERE id =?"
+        cursor = get_db().cursor()
+        cursor.execute(user)
+        username = cursor.fetch(1)
         pass
     return False
 
-def logout_user():
-    session.pop('user')'''
+@app.route('/')
+def index():
+    '''make an app route and link to index html template'''
+    return render_template('index.html')
 
-# sign up and insert information into database
+
+
+
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
+    '''sign up and insert information into database'''
     if request.method == "GET" :
         return render_template("signup.html")
     error=None
@@ -72,14 +67,11 @@ def signup():
         error = "Username already taken. Please try again"
         return render_template("signup.html", error=error)
     sql_query = "INSERT INTO login (username, password) VALUES (?,?)"
+    '''insert new username and password into database'''
     cursor.execute(sql_query, (new_username, generate_password_hash(new_password, )))
     get_db().commit()
-    sql_query = "SELECT ID FROM login WHERE username = ?"
-    cursor.execute(sql_query, (new_username,))
-    global user_id
-    user_id = cursor.fetchall()[0][0]
-    session["logged_in"] = True
     return redirect("/")
+    
 
 # find and use code from database to access user functions on site
 @app.route('/login', methods=["GET", "POST"])
@@ -96,14 +88,15 @@ def login():
     if bool(results):
         if check_password_hash(results[0][1], password):
             session["logged_in"] = True
-            global user_id
             user_id = results[0][0]
             return redirect('/')
     if username == '' and password == '':
         return redirect('/')
     if username == '' or password == '' or username.isspace():
         error = "Please enter a valid username and password."
-        return render_template("login.html", error=error)
+    else:
+        error = "Please enter a valid username and password."
+    return render_template("login.html", error=error)
 
 # log out of site
 @app.route('/logout')
@@ -120,6 +113,8 @@ def menu():
 # ordering system using database and foreign keys
 @app.route("/drink_order", methods=["GET", "POST"])
 def order():
+    if 'logged_in' not in session:
+        return redirect("/login")
     extras = "SELECT id, name FROM additives"
     cursor = get_db().cursor()
     cursor.execute(extras)
@@ -135,24 +130,29 @@ def order():
     cursor.execute(drinks)
     flavour = cursor.fetchall()
 
+    customer_id = "SELECT id, username FROM login;"
+    cursor = get_db().cursor()
+    cursor.execute(customer_id)
+    name = cursor.fetchall()
+
     items = "SELECT username, name, temperature, additives FROM drink_order;"
     cursor = get_db().cursor()
     cursor.execute(items)
     goods = cursor.fetchall()
 
-    return render_template('drink_order.html', temperature=temperature, additives=additives,  flavour=flavour, goods=goods, admin = admin)
+    return render_template('drink_order.html', temperature=temperature, additives=additives,  flavour=flavour, goods=goods, name=name, admin = admin, user_id=user_id)
 
 # add order to drink order
 @app.route("/add", methods={"GET", "POST"})
 def add():
     if request.method == "POST":
         cursor = get_db().cursor()
-        customer_name = request.form['customer_name']
+        user_id = username
         drink_name = request.form['drink_name']
         drink_temperature = request.form["drink_temperature"]
         drink_additives = request.form["drink_additives"]
         sql = "INSERT INTO drink_order(username, name, temperature, additives) VALUES (?,?,?,?)"
-        cursor.execute(sql,(customer_name, drink_name, drink_temperature, drink_additives))
+        cursor.execute(sql,(user_id, drink_name, drink_temperature, drink_additives))
         get_db().commit()
     return redirect("/drink_order")
 
@@ -163,7 +163,7 @@ def delete():
         return redirect("/drink_order")
     if request.method == 'POST':
         cursor = get_db().cursor()
-        customer_username = str(request.form["customer_name"])
+        customer_username = int(request.form["customer_name"])
         sql = "DELETE FROM drink_order WHERE id=?"
         cursor.execute(sql,(customer_username,))
         get_db().commit()
